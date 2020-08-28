a304a61843865aa177344b27805a5110e5f48164
Wdp <wdpmobileteam@gmail.com>
2020-08-28 11:40:22 +0530

    Initial Commit

---
+++ SendBirdChatExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendBirdChatExample
+//
+//  Created by WDP on 28/08/20.
+//  Copyright © 2020 WDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
